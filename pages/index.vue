1bd9051737dd1f3dba6291e14dab918930fadb15
cjkjunior <junior@DESKTOP-5604V3H.localdomain>
2022-06-23 15:55:53 -0300

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <Html :lang="dynamic > 50 ? 'en-GB' : 'en_US'">
+      <Head>
+        <Title>Restaurantly</Title>
+        <Meta name="desc" content="page"/>
+        <Link rel="preload" href="https://w7.pngwing.com/pngs/672/907/png-transparent-favicon-buggi-trademark-logo-circle.png" as="script"/>
+      </Head>
+    </Html>
+    <div class="container">
+      <h1>Welcome to Restaurantly</h1>
+      <a href="/restaurants">Go to restaurants</a>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+  .container {
+    text-align: center;
+    margin-top: 5rem;
+  }
+
+</style>
---
+++ pages/restaurants/index.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+    <Html :lang="dynamic > 50 ? 'en-GB' : 'en_US'">
+      <Head>
+        <Title>Top 50 Restaurants</Title>
+        <Meta name="desc" content="page"/>
+        <Link rel="preload" href="https://w7.pngwing.com/pngs/672/907/png-transparent-favicon-buggi-trademark-logo-circle.png" as="script"/>
+      </Head>
+    </Html>
+        <NuxtLayout name="custom">
+            <div class="container">
+                <RestaurantTable />
+            </div>
+        </NuxtLayout>
+    </div>
+</template>
